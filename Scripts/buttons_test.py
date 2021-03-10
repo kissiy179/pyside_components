f661625a70079b7e421b44981c5ded60c459dc81
kissiy179 <kissiy179@gmail.com>
2021-03-10 21:57:16 +0900

    buttons_test.pyのRemovableButton, TagButton部分をより機能がわかりやす用に機能追加

--- Scripts/buttons_test.py
+++ Scripts/buttons_test.py
@@ -2,6 +2,7 @@
 import sys
 from functools import partial
 from qtpy import QtCore, QtWidgets
+import qtawesome as qta
 from pyside_components.widgets.double_clickable_button import DoubleClickableButton
 from pyside_components.widgets.text_editable_button import TextEditableButton
 from pyside_components.widgets.removable_button import RemovableButton
@@ -39,24 +40,68 @@ class TestDialog(QtWidgets.QDialog):
         lo.addWidget(btn)
 
         # Removable button
+        hlo = QtWidgets.QHBoxLayout()
+        self.removable_btn_lo = QtWidgets.QVBoxLayout()
+        hlo.addLayout(self.removable_btn_lo)
         btn = RemovableButton()
         btn.clicked.connect(partial(print_button_text, btn))
         btn.closed.connect(partial(print_, 'closed!'))
         btn.setText(type(btn).__name__)
-        lo.addWidget(btn)
+        self.removable_btn_lo.addWidget(btn)
+
+        add_icon = qta.icon('fa5s.plus', color='gray')
+        add_btn = QtWidgets.QPushButton()
+        add_btn.setStyleSheet('background-color: rgba(1,1,1,0);')
+        add_btn.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        add_btn.setIcon(add_icon)
+        add_btn.clicked.connect(self.add_removable_button)
+        hlo.addWidget(add_btn, alignment=QtCore.Qt.AlignTop | QtCore.Qt.AlignRight)
+
+        lo.addLayout(hlo)
 
         # Tag button
+        self.tag_btn_le = QtWidgets.QLineEdit()
+        # self.tag_btn_le.setStyleSheet('border-radius: 5px')
+        self.tag_btn_le.setPlaceholderText('Add tag...')
+        self.tag_btn_le.editingFinished.connect(self.add_tag_button)
+        lo.addWidget(self.tag_btn_le)
+
+        self.tag_btn_lo = QtWidgets.QVBoxLayout()
         btn = TagButton()
         btn.clicked.connect(partial(print_button_text, btn))
         btn.closed.connect(partial(print_, 'closed!'))
         btn.text_changed.connect(partial(print_, 'text changed!!'))
         btn.setText(type(btn).__name__)
-        lo.addWidget(btn)
+        self.tag_btn_lo.addWidget(btn)
+
+        lo.addLayout(self.tag_btn_lo)
 
         # Spacer
         spacer = QtWidgets.QSpacerItem(0, 0, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         lo.addItem(spacer)
         self.setLayout(lo)
+
+    def add_removable_button(self):
+        btn = RemovableButton()
+        btn.clicked.connect(partial(print_button_text, btn))
+        btn.closed.connect(partial(print_, 'closed!'))
+        btn.setText(type(btn).__name__)
+        self.removable_btn_lo.addWidget(btn)
+
+    def add_tag_button(self):
+        text = self.tag_btn_le.text()
+        
+        if not text:
+            return
+        
+        self.tag_btn_le.setText('')
+        btn = TagButton()
+        btn.clicked.connect(partial(print_button_text, btn))
+        btn.closed.connect(partial(print_, 'closed!'))
+        btn.setText(text)
+        self.tag_btn_lo.addWidget(btn)
+
+        
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
