ab8abbfccb2d2a9e1232aa197a25da1dfd14c0e6
kissiy179 <kissiy179@gmail.com>
2021-03-10 14:52:11 +0900

    TextEditableButtonにtext_changedシグナルを追加

--- python/pyside_components/widgets/text_editable_button.py
+++ python/pyside_components/widgets/text_editable_button.py
@@ -3,12 +3,14 @@ from qtpy import QtCore, QtGui, QtWidgets
 from .double_clickable_button import DoubleClickableButton
 
 class TextEditableButton(DoubleClickableButton):
+
+    text_changed = QtCore.Signal()
     
     def __init__(self, text='', parent=None):
         super(DoubleClickableButton, self).__init__(text, parent)
-        self.setup_ui()
+        self.add_lineedit()
 
-    def setup_ui(self):
+    def add_lineedit(self):
         self.lineedit = QtWidgets.QLineEdit(self)
         self.lineedit.setContentsMargins(1,1,1,1)
         self.lineedit.hide()
@@ -16,8 +18,12 @@ class TextEditableButton(DoubleClickableButton):
         self.double_clicked.connect(self.to_editable)
 
     def resizeEvent(self, e):
-        self.lineedit.resize(self.size())
         super(TextEditableButton, self).resizeEvent(e)
+        self.lineedit.resize(self.size())
+
+    def setText(self, s):
+        super(TextEditableButton, self).setText(s)
+        self.text_changed.emit()
 
     def set_text(self):
         text = self.lineedit.text()
