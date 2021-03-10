486c91cb6b6b05878cc73c08980cbdb1e6d01834
kissiy179 <kissiy179@gmail.com>
2021-03-11 01:27:40 +0900

    デバッグ用に.pyファイル実行バッチファイルを管理者権限で実行した場合最後にpauseがかかるように変更

--- Scripts/excec_py.bat
+++ Scripts/excec_py.bat
@@ -1,5 +1,10 @@
 @echo off
 
-cd %~dp0
+cd /d %~dp0
 call ..\.venv\scripts\activate.bat
 call python %1
+
+@REM 管理者権限実行の場合ポーズする
+openfiles > NUL 2>&1 
+
+if %ERRORLEVEL%==0 pause
--- Scripts/flow_layout_test.bat
+++ Scripts/flow_layout_test.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cd %~dp0
+cd /d %~dp0
 call excec_py.bat flow_layout_test.py
