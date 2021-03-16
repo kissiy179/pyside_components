20d6feda8a9f1d745dab059b5d7ce2641cc7a7d8
kissiy179 <kissiy179@gmail.com>
2021-03-17 08:09:20 +0900

    TagItemButtonの色を認識しやすく修正

--- python/pyside_components/widgets/tag_item_button.py
+++ python/pyside_components/widgets/tag_item_button.py
@@ -46,16 +46,24 @@ def char_to_color(c):
     col_num = int(hex_code, 16)    
     return col_num
 
-def string_to_color(s):
+def string_to_color(s, correction=40):
     s = s if s else 'z'
-    s = s.ljust(3, s[0])
+    s = s.ljust(3, '0')
     color = [char_to_color(i) for i in s[:3]]
-    n = 2
+    avg = sum(color) / len(color)
+
+    if avg < correction:
+        sub = correction - avg
+        color = [min(255, n + sub) for n in color]
+
     max_ = max(color)
     min_ = min(color)
     
-    if max_ - min_ <= 20:
-        color[0] = min(255, color[0] * n)
+    if max_ - min_ <= 255:
+        maxidx = color.index(max_)
+        minidx = color.index(min_)
+        color[maxidx] = min(255, max_ + correction)
+        color[minidx] = max(0, min_ - correction)
         
     return color
 
