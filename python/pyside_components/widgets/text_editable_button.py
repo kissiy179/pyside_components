29b1ed04ff62fb5a51c14f8396435e5555a4a243
kissiy179 <kissiy179@gmail.com>
2021-03-22 00:42:50 +0900

    TextEditableButtonの機能をTextEditableButtonMixinに分離

--- python/pyside_components/widgets/text_editable_button.py
+++ python/pyside_components/widgets/text_editable_button.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from qtpy import QtCore, QtGui, QtWidgets
-from .double_clickable_button import DoubleClickableButton
+from .double_clickable_button import DoubleClickableButtonMixin
 
-class TextEditableButton(DoubleClickableButton):
-
+class TextEditableButtonMixin(DoubleClickableButtonMixin):
+    
     text_changed = QtCore.Signal()
     
-    def __init__(self, text='', parent=None):
-        super(DoubleClickableButton, self).__init__(text, parent)
+    def __init__(self, *args, **kwargs):
+        super(TextEditableButtonMixin, self).__init__(*args, **kwargs)
         self.add_lineedit()
 
     def add_lineedit(self):
@@ -18,11 +18,11 @@ class TextEditableButton(DoubleClickableButton):
         self.double_clicked.connect(self.to_editable)
 
     def resizeEvent(self, e):
-        super(TextEditableButton, self).resizeEvent(e)
+        super(TextEditableButtonMixin, self).resizeEvent(e)
         self.lineedit.resize(self.size())
 
     def setText(self, s):
-        super(TextEditableButton, self).setText(s)
+        super(TextEditableButtonMixin, self).setText(s)
         self.text_changed.emit()
 
     def set_text(self):
@@ -38,3 +38,8 @@ class TextEditableButton(DoubleClickableButton):
         self.lineedit.show()
         self.lineedit.selectAll()
         self.lineedit.setFocus()
+
+class TextEditableButton(TextEditableButtonMixin, QtWidgets.QPushButton):
+    
+    def __init__(self, text='', parent=None):
+        super(TextEditableButton, self).__init__(text, parent)
