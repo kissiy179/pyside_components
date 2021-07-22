b6e5b6a4bdbbb03eeb5fa162bc49f6802b404096
kissiy179 <kissiy179@gmail.com>
2021-07-23 02:51:43 +0900

    RemovableButtonをDoubleActionsButtonMixinを継承する形に変更

--- python/pyside_components/widgets/removable_button.py
+++ python/pyside_components/widgets/removable_button.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from qtpy import QtCore, QtWidgets
 import qtawesome as qta
+from .double_actions_button import DoubleActionsButton, DoubleActionsButtonMixin
 
 removable_btn_style = '''
 {class_name} {{
@@ -11,6 +12,9 @@ removable_btn_style = '''
 close_btn_style = '''
 border: 0px;
 padding: 0px;
+background-color: rgba(0,0,0,0);
+'''
+close_btn_style = '''
 background-color: rgba(0,0,0,0);
 '''
 
@@ -29,40 +33,24 @@ class CloseButton(QtWidgets.QPushButton):
         super(CloseButton, self).__init__(text, parent)
         icons = get_icons(icon_color)
         self.setIcon(icons.get('close'))
-        self.setIconSize(QtCore.QSize(16, 16))
+        self.setIconSize(QtCore.QSize(14, 14))
         self.setStyleSheet(close_btn_style)
 
     def keyPressEvent(self, e):
         return
 
-class RemovableButtonMixin(object):
+class RemovableButtonMixin(DoubleActionsButtonMixin):
 
     closed = QtCore.Signal()
     icon_color = 'gray'
 
     def __init__(self, *args, **kwargs):
         super(RemovableButtonMixin, self).__init__(*args, **kwargs)
-        self.add_close_button()
 
-    def add_close_button(self):
-        self.close_btn = CloseButton(icon_color=self.icon_color, parent=self)
-        self.close_btn.clicked.connect(self.close)
-        btn_size = self.close_btn.sizeHint()
-        default_padding = self.style().pixelMetric(QtWidgets.QStyle.PM_ButtonMargin)
-        frame_width = self.style().pixelMetric(QtWidgets.QStyle.PM_DefaultFrameWidth)
-        padding = default_padding - frame_width 
-        self.setStyleSheet(removable_btn_style.format(class_name=type(self).__name__,
-                                                      padding_left=padding,
-                                                      padding_right=btn_size.width() + padding,
-                                                      padding_top=padding,
-                                                      padding_bottom=padding))
-
-    def resizeEvent(self, event):
-        super(RemovableButtonMixin, self).resizeEvent(event)
-        btn_size = self.close_btn.sizeHint()
-        frame_width = self.style().pixelMetric(QtWidgets.QStyle.PM_DefaultFrameWidth)
-        self.close_btn.move(self.rect().right() - frame_width - btn_size.width(),
-                            (self.rect().bottom() - btn_size.height() + frame_width * 2) /2)
+    def add_inner_button(self):
+        btn = CloseButton(icon_color=self.icon_color, parent=self)
+        btn.clicked.connect(self.close)
+        return btn
 
     def close(self):
         super(RemovableButtonMixin, self).close()
