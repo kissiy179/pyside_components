b67fbbc8e83d50ae3ace99c27e24db5a572fc2ba
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2021-11-24 00:12:22 +0900

    setup.pyにmayapyからの実行だった場合に無視するパッケージリストを追加

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 import sys
 from glob import glob
 from os.path import basename
@@ -6,25 +7,35 @@ from os.path import exists
 from setuptools import setup
 from setuptools import find_packages
 
-def get_requires_from_file(filename):
+# List of packages to ignore.
+# Pre-installed in Maya, etc.
+IGNORE_PACKAGES = ['PySide', 'PySide2']
+
+# Is the Python running in Maya?
+IS_MAYA_PYTHON = 'maya' in sys.prefix.lower()
+
+def _requires_from_file(filename):
     if not exists(filename):
         return []
+
+    requires = {}
     
-    return open(filename).read().splitlines()
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-def get_requires():
-    requires = []
-    
-    if sys.version[0] == '2':
-        requires = get_requires_from_file('requirements2.txt')
+        for line in lines:
+            line = line.strip('\n')
+            package_name = line.split(' ')[0]
 
-    if not requires:
-        requires = get_requires_from_file('requirements.txt')
+            if IS_MAYA_PYTHON and package_name in IGNORE_PACKAGES:
+                continue
 
-    return requires
+            requires[line] = line
+
+    return list(requires.keys())
 
 setup(
-    name='pyside_components',
+    name='pysid_components',
     version='0.1.0',
     package_dir={"": "python"},
     packages=find_packages("python"),
@@ -32,5 +43,5 @@ setup(
     include_package_data=True,
     zip_safe=False,
     python_requires=">=2.7",
-    install_requires=get_requires(),
+    install_requires=_requires_from_file('requirements.txt'),
 )
