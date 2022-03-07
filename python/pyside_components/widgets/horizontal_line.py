c3ad4053ee34ae9e8bd88266916ca3108f22bfcb
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2022-03-07 10:59:46 +0900

    HorizontalLineを追加

---
+++ python/pyside_components/widgets/horizontal_line.py
@@ -0,0 +1,9 @@
+from qtpy import *
+
+class HorizontalLine(QtWidgets.QFrame):
+
+    def __init__(self, *args, **kwargs):
+        super(HorizontalLine, self).__init__(*args, **kwargs)
+        self.setFrameShape(QtWidgets.QFrame.HLine)
+        self.setFrameShadow(QtWidgets.QFrame.Plain)
+
