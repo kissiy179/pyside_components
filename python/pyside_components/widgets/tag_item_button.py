a726b22d891f144173eb098c0725e66eae9045ac
kissiy179 <kissiy179@gmail.com>
2022-04-26 22:57:40 +0900

    string_to_color関数をutil.colorに移動

--- python/pyside_components/widgets/tag_item_button.py
+++ python/pyside_components/widgets/tag_item_button.py
@@ -37,37 +37,6 @@ QLineEdit {{
 }}
 '''
 
-def char_to_color(c):
-    if sys.version.startswith('3'):
-        hex_code = codecs.encode(bytes(c, 'utf-8'), 'hex-codec')
-
-    else:
-        hex_code = codecs.encode(c.lower(), 'hex')
-        
-    col_num = int(hex_code, 16)    
-    return col_num
-
-def string_to_color(s, correction=40):
-    s = s if s else 'z'
-    s = s.ljust(3, '0')
-    color = [char_to_color(i) for i in s[:3]]
-    avg = sum(color) / len(color)
-
-    if avg < correction:
-        sub = correction - avg
-        color = [min(255, n + sub) for n in color]
-
-    max_ = max(color)
-    min_ = min(color)
-    
-    if max_ - min_ <= 255:
-        maxidx = color.index(max_)
-        minidx = color.index(min_)
-        color[maxidx] = min(255, max_ + correction)
-        color[minidx] = max(0, min_ - correction)
-        
-    return color
-
 class TagItemButton(RemovableButtonMixin, TextEditableButtonMixin, QtWidgets.QPushButton):
 
     icon_color = 'whitesmoke'
