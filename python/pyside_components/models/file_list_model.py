7e6a051cb911c6d3916d15f1807b85a3fe211c6d
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2022-06-03 19:42:47 +0900

    FileListModelのファイル検索にプログレスバーを表示するように変更

--- python/pyside_components/models/file_list_model.py
+++ python/pyside_components/models/file_list_model.py
@@ -31,6 +31,7 @@ class FileListDB(object):
     __filters = []
     __result = []
     __engine = None
+    __session = None
 
     def __init__(self, root_dir_path='', filters=()):
         super(FileListDB, self).__init__()
@@ -56,28 +57,45 @@ class FileListDB(object):
         return self.__result
 
     def build(self, root_dir_path):
+        # if self.__session:
+        #     self.__session.close()
+            # self.__session.dispose()
+
         self.__engine = create_engine('sqlite:///:memory:', echo=echo)
         Base.metadata.create_all(self.__engine)
         SessionClass = sessionmaker(bind=self.__engine)
-        session = SessionClass()
+        self.__session = SessionClass()
+
+        file_paths = []
 
         for dir_path, dir_names, file_names in os.walk(root_dir_path):
             for file_name in file_names:
                 file_path = os.path.join(dir_path, file_name)
-                file_path_, ext = os.path.splitext(file_path)
-                file_info = FileInfo(
-                    file_path = file_path,
-                    extention = ext,
-                )
 
-                try:
-                    session.add(file_info)
+                file_paths.append(file_path)
 
-                except Exception as e:
-                    print(e)
+        app = QtWidgets.QApplication
+        prog = QtWidgets.QProgressDialog('Search files...', None, 0, len(file_paths), None, 0)
+        prog.setWindowFlags(QtCore.Qt.Popup)
+        prog.show()
 
-        session.commit()
-        session.close()
+        for i, file_path in enumerate(file_paths):
+            file_path_, ext = os.path.splitext(file_path)
+            file_info = FileInfo(
+                file_path = file_path,
+                extention = ext,
+            )
+            prog.setValue(i)
+            app.processEvents()
+
+            try:
+                self.__session.add(file_info)
+
+            except Exception as e:
+                print(e)
+
+        self.__session.commit()
+        # self.__session.close()
 
     def filter(self, filters=()):
         ext_filters = []
@@ -103,16 +121,16 @@ class FileListDB(object):
                     f_ = FileInfo.file_path.like('%{}%'.format(f))
                     normal_filters.append(f_)
 
-        SessionClass = sessionmaker(bind=self.__engine)
-        session = SessionClass()
-        query = session.query(FileInfo).filter(
+        # SessionClass = sessionmaker(bind=self.__engine)
+        # self.__session = SessionClass()
+        query = self.__session.query(FileInfo).filter(
             and_(*normal_filters),
             or_(*ext_filters)
         ).all()
 
         self.__result = [row.file_path for row in query]
-        session.commit()
-        session.close()
+        # self.__session.commit()
+        # self.__session.close()
 
 # class FileListModel(QtGui.QStandardItemModel):
 class FileListModel(QtGui.QStringListModel):
@@ -125,6 +143,8 @@ class FileListModel(QtGui.QStringListModel):
     def __init__(self, root_dir_path='', filters=(), parent=None):
         super(FileListModel, self).__init__(parent)
         self.__db = FileListDB(root_dir_path, filters)
+        file_paths = self.__db.get_result()
+        self.setStringList(file_paths)
 
     def __getattr__(self, attrname):
         attr = getattr(self.__db, attrname)
