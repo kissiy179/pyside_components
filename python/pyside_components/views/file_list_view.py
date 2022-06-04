cbbefabe2864fc88ad6ab6f47162394843d995fe
kissiy179 <kissiy179@gmail.com>
2022-06-05 00:39:12 +0900

    FileListViewにコメントを追加

--- python/pyside_components/views/file_list_view.py
+++ python/pyside_components/views/file_list_view.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 from qtpy import QtCore, QtGui, QtWidgets
 import pyside_components.widgets.path_edit as path_edit
 
 class FileListView(QtWidgets.QTreeView):
+    '''
+    FileListModelを表示することを想定したビュークラス
+    '''
 
     def __init__(self, parent=None):
         super(FileListView, self).__init__(parent)
@@ -12,7 +16,10 @@ class FileListView(QtWidgets.QTreeView):
         column_count = model.columnCount()
                 
         for i in range(column_count):
+            # コンテンツ幅に合わせて列幅を指定
             self.resizeColumnToContents(i)
+
+            # コンテンツ幅に合わせて列幅を自動調整する設定(重いのでコメントアウト)
             # header.setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeToContents)
 
     def set_root_dir_path(self, root_dir_path):
